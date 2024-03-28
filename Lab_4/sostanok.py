2cef358c7413bde8d3e26f7bc0f69124b64e15b5
Skipio <davidstojkov23@gmail.com>
2024-03-28 19:45:27 +0100

    lab 4 - 1 added, Проблеми кои исполнуваат услови

--- Lab_4/sostanok.py
+++ Lab_4/sostanok.py
@@ -16,9 +16,16 @@ if __name__ == '__main__':
     problem.addConstraint(lambda a, b, c:
                           ((a == 1 and c in [14, 15, 18]) or (a == 0 and c not in [14, 15, 18])) and
                           ((b == 1 and c in [12, 13, 16, 17, 18, 19, 20]) or (b == 0 and c not in [12, 13, 16, 17, 18])),
-                          ("Petar_prisustvo", "Marija_prisustvo", "vreme_sostanok"))
+                          ("Marija_prisustvo", "Petar_prisustvo", "vreme_sostanok"))
     problem.addConstraint(lambda a, b, c: a == 1 and (b == 1 or c == 1),
                           ("Simona_prisustvo", "Marija_prisustvo", "Petar_prisustvo"))
     # ----------------------------------------------------
 
-    [print(solution) for solution in problem.getSolutions()]
+    # [print(solution) for solution in problem.getSolutions()]
+    for solution in problem.getSolutions():
+        print('{', end='')
+        print(f"'Simona_prisustvo': {solution.get('Simona_prisustvo')}, ", end="")
+        print(f"'Marija_prisustvo': {solution.get('Marija_prisustvo')}, ", end="")
+        print(f"'Petar_prisustvo': {solution.get('Petar_prisustvo')}, ", end="")
+        print(f"'vreme_sostanok': {solution.get('vreme_sostanok')}", end="")
+        print('}')
